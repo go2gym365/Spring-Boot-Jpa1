0807c85bd02a944fd7db1c383273744cc3a48842
Boiling SODA <go2gym365@gmail.com>
2024-01-24 10:58:15 +0000

    feat: Domain design

--- jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jpabook.jpashop.domain.item.Item;
@@ -27,16 +27,23 @@ public class Category {
 
     private String name;
 
-    @ManyToMany
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinTable(name = "category_item", 
             joinColumns = @JoinColumn(name = "category_id"), 
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+
+    //===연관관계 메서드===//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
